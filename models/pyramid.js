13c6fc24c8ecd5bd0cb8f4b22df1ab2ce9e2c8ae
Mark Thill <mrt8449@rit.edu>
2013-05-16 17:33:55 -0400

    Changed boid model.

--- models/pyramid.js
+++ models/pyramid.js
@@ -1,29 +1,49 @@
     setupPyramidBuffers = function (pyramidVertexPositionBuffer,pyramidVertexColorBuffer,gl) {
         gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
         var vertices = [
-            // Front face
+            // Front face1
              0.0,  1.0,  0.0,
             -1.0, -1.0,  1.0,
              1.0, -1.0,  1.0,
 
-            // Right face
+            // Right face1
              0.0,  1.0,  0.0,
              1.0, -1.0,  1.0,
              1.0, -1.0, -1.0,
 
-            // Back face
+            // Back face1
              0.0,  1.0,  0.0,
              1.0, -1.0, -1.0,
             -1.0, -1.0, -1.0,
 
-            // Left face
+            // Left face1
              0.0,  1.0,  0.0,
             -1.0, -1.0, -1.0,
-            -1.0, -1.0,  1.0
+            -1.0, -1.0,  1.0,
+
+             // Front face2
+             0.0, -5.0,  0.0,
+             1.0, -1.0,  1.0,
+            -1.0, -1.0,  1.0,
+
+            // Right face2
+             0.0,  -5.0,  0.0,
+             1.0,  -1.0, -1.0,
+             1.0,  -1.0,  1.0,
+
+            // Back face2
+             0.0,  -5.0,  0.0,
+             -1.0, -1.0, -1.0,
+             1.0,  -1.0, -1.0,
+
+            // Left face
+             0.0,  -5.0,  0.0,
+             -1.0, -1.0,  1.0,
+            -1.0, -1.0, -1.0
         ];
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
         pyramidVertexPositionBuffer.itemSize = 3;
-        pyramidVertexPositionBuffer.numItems = 12;
+        pyramidVertexPositionBuffer.numItems = 24;
 
         //pyramidVertexColorBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexColorBuffer);
@@ -46,9 +66,29 @@
             // Left face
             1.0, 1.0, 0.0, 1.0,
             1.0, 1.0, 0.0, 1.0,
+            1.0, 1.0, 0.0, 1.0,
+
+            // Front face
+            1.0, 0.0, 0.0, 1.0,
+            1.0, 0.0, 0.0, 1.0,
+            1.0, 0.0, 0.0, 1.0,
+
+            // Right face
+            0.0, 1.0, 0.0, 1.0,
+            0.0, 1.0, 0.0, 1.0,
+            0.0, 1.0, 0.0, 1.0,
+
+            // Back face
+            0.0, 0.0, 1.0, 1.0,
+            0.0, 0.0, 1.0, 1.0,
+            0.0, 0.0, 1.0, 1.0,
+
+            // Left face
+            1.0, 1.0, 0.0, 1.0,
+            1.0, 1.0, 0.0, 1.0,
             1.0, 1.0, 0.0, 1.0
         ];
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
         pyramidVertexColorBuffer.itemSize = 4;
-        pyramidVertexColorBuffer.numItems = 12;
+        pyramidVertexColorBuffer.numItems = 24;
     };
